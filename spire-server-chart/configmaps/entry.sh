e04d5d3dadae5d892f7e5e008df76cbe943f5762
Vishnu Soman <vishnu@accuknox.com>
2023-04-05 10:56:54 +0530

    Updated helm charts for spire agent and server
    Signed-off-by: Vishnu Soman <vishnu@accuknox.com>

---
+++ spire-server-chart/configmaps/entry.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+SOCKET_FILE="/run/spire-server/private/api.sock"
+
+if [[ -S "$SOCKET_FILE" ]]; then
+
+   /bin/spire-server entry create                                       \
+        -parentID spiffe://accuknox.com/saas/agent                      \
+        -spiffeID spiffe://accuknox.com/saas/cluster-onboarding         \
+        -selector k8s_sat:sa:cluster-onboarding                         \
+        -selector k8s_sat:pod-label:app:cluster-onboarding              \
+        -socketPath $SOCKET_FILE                                    &&  \
+   /bin/spire-server entry create                                       \
+        -parentID spiffe://accuknox.com/saas/agent                      \
+        -spiffeID spiffe://accuknox.com/saas/knox-gateway               \
+        -selector k8s_sat:pod-label:app:knox-gateway                    \
+        -selector k8s_sat:pod-label:environment:dev                     \
+        -selector k8s_sat:sa:knox-gateway                               \
+        -socketPath $SOCKET_FILE                                    &&  \
+   /bin/spire-server entry create                                       \
+        -parentID spiffe://accuknox.com/saas/agent                      \
+        -spiffeID spiffe://accuknox.com/saas/policyprovider-service     \
+        -selector k8s_sat:pod-label:app:policy-provider-service         \
+        -selector k8s_sat:pod-label:environment:dev                     \
+        -selector k8s_sat:sa:policyprovider-service                     \
+        -socketPath $SOCKET_FILE                                    &&  \
+   rm -rf /bin/spire-server
+
+fi
